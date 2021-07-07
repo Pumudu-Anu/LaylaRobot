f88e309fbc799ff2d63d65c35c8f2268c1961e7f
Pumudu-anuradha <85781602+Pumudu-anuradha@users.noreply.github.com>
2021-07-07 13:51:20 +0530

    Update __songs.py

--- LaylaRobot/modules/__songs.py
+++ LaylaRobot/modules/__songs.py
@@ -10,149 +10,104 @@ from LaylaRobot import pbot as Jebot
 from LaylaRobot.pyrogramee.dark import get_arg
 
 
-@Jebot.on_message(filters.command("saavn"))
-async def song(client, message):
-    message.chat.id
-    message.from_user["id"]
-    args = get_arg(message) + " " + "song"
-    if args.startswith(" "):
-        await message.reply("<b>Enter song name❗</b>")
-        return ""
-    m = await message.reply_text(
-        "Downloading your song,\nPlz wait ⏳️"
-    )
+@Jebot.on_message(filters.command(["lyric", "lyrics"]))
+async def _(client, message):
+    lel = await message.reply("Searching For Lyrics.....")
+    query = message.text
+    if not query:
+        await lel.edit("`What I am Supposed to find `")
+        return
+
+    song = ""
+    song = Song.find_song(query)
+    if song:
+        if song.lyrics:
+            reply = song.format()
+        else:
+            reply = "Couldn't find any lyrics for that song! try with artist name along with song if still doesnt work try `.glyrics`"
+    else:
+        reply = "lyrics not found! try with artist name along with song if still doesnt work try `.glyrics`"
+
+    if len(reply) > 4095:
+        with io.BytesIO(str.encode(reply)) as out_file:
+            out_file.name = "lyrics.text"
+            await client.send_document(
+                message.chat.id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption=query,
+                reply_to_msg_id=message.message_id,
+            )
+            await lel.delete()
+    else:
+        await lel.edit(reply)  # edit or reply
+
+
+@Jebot.on_message(filters.command(["glyric", "glyrics"]))
+async def lyrics(client, message):
+
+    if r"-" in message.text:
+        pass
+    else:
+        await message.reply(
+            "`Error: please use '-' as divider for <artist> and <song>`\n"
+            "eg: `.glyrics Nicki Minaj - Super Bass`"
+        )
+        return
+
+    if GENIUS is None:
+        await message.reply(
+            "`Provide genius access token to config.py or Heroku Config first kthxbye!`"
+        )
+    else:
+        genius = lyricsgenius.Genius(GENIUS)
+        try:
+            args = message.text.split(".lyrics")[1].split("-")
+            artist = args[0].strip(" ")
+            song = args[1].strip(" ")
+        except Exception:
+            await message.reply("`Lel please provide artist and song names`")
+            return
+
+    if len(args) < 1:
+        await message.reply("`Please provide artist and song names`")
+        return
+
+    lel = await message.reply(f"`Searching lyrics for {artist} - {song}...`")
+
     try:
-        r = requests.get(f"https://jostapi.herokuapp.com/saavn?query={args}")
-    except Exception as e:
-        await m.edit(str(e))
+        songs = genius.search_song(song, artist)
+    except TypeError:
+        songs = None
+
+    if songs is None:
+        await lel.edit(f"Song **{artist} - {song}** not found!")
         return
-    sname = r.json()[0]["song"]
-    slink = r.json()[0]["media_url"]
-    ssingers = r.json()[0]["singers"]
-    file = wget.download(slink)
-    ffile = file.replace("mp4", "m4a")
-    os.rename(file, ffile)
-    await message.reply_audio(audio=ffile, title=sname, performer=ssingers)
-    os.remove(ffile)
-    await m.delete()
+    if len(songs.lyrics) > 4096:
+        await lel.edit("`Lyrics is too big, view the file to see it.`")
+        with open("lyrics.txt", "w+") as f:
+            f.write(f"Search query: \n{artist} - {song}\n\n{songs.lyrics}")
+        await client.send_document(
+            message.chat.id,
+            "lyrics.txt",
+            reply_to_msg_id=message.message_id,
+        )
+        os.remove("lyrics.txt")
+    else:
+        await lel.edit(
+            f"**Search query**: \n`{artist} - {song}`\n\n```{songs.lyrics}```"
+        )
+    return
 
 
-#deezer#
-# Credits for @TheHamkerCat
-
-import os
-import aiofiles
-import aiohttp
-from pyrogram import filters
-from LaylaRobot import pbot as Layla
-
-ARQ = "https://thearq.tech/"
-
-async def fetch(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            try:
-                data = await resp.json()
-            except:
-                data = await resp.text()
-    return data
-
-async def download_song(url):
-    song_name = f"asuna.mp3"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            if resp.status == 200:
-                f = await aiofiles.open(song_name, mode="wb")
-                await f.write(await resp.read())
-                await f.close()
-    return song_name
-
-
-@Layla.on_message(filters.command("deezer"))
-async def deezer(_, message):
-    if len(message.command) < 2:
-        await message.reply_text("Download Now Deezer")
-        return
-    text = message.text.split(None, 1)[1]
-    query = text.replace(" ", "%20")
-    m = await message.reply_text("Searching...")
-    try:
-        r = await fetch(f"{ARQ}deezer?query={query}&count=1")
-        title = r[0]["title"]
-        url = r[0]["url"]
-        artist = r[0]["artist"]
-    except Exception as e:
-        await m.edit(str(e))
-        return
-    await m.edit("Downloading...")
-    song = await download_song(url)
-    await m.edit("Uploading...")
-    await message.reply_audio(audio=song, title=title, performer=artist)
-    os.remove(song)
-    await m.delete()
-
-#Deezer
-# Credits for @TheHamkerCat
-
-import os
-import aiofiles
-import aiohttp
-from pyrogram import filters
-from LaylaRobot import pbot as ASUNA
-
-ARQ = "https://thearq.tech/"
-
-async def fetch(url):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            try:
-                data = await resp.json()
-            except:
-                data = await resp.text()
-    return data
-
-async def download_song(url):
-    song_name = f"asuna.mp3"
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as resp:
-            if resp.status == 200:
-                f = await aiofiles.open(song_name, mode="wb")
-                await f.write(await resp.read())
-                await f.close()
-    return song_name
-
-
-@Layla.on_message(filters.command("deezer"))
-async def deezer(_, message):
-    if len(message.command) < 2:
-        await message.reply_text("Download Now Deezer")
-        return
-    text = message.text.split(None, 1)[1]
-    query = text.replace(" ", "%20")
-    m = await message.reply_text("Searching...")
-    try:
-        r = await fetch(f"{ARQ}deezer?query={query}&count=1")
-        title = r[0]["title"]
-        url = r[0]["url"]
-        artist = r[0]["artist"]
-    except Exception as e:
-        await m.edit(str(e))
-        return
-    await m.edit("Downloading...")
-    song = await download_song(url)
-    await m.edit("Uploading...")
-    await message.reply_audio(audio=song, title=title, performer=artist)
-    os.remove(song)
-    await m.delete()
-    
-    
-__mod_name__ = "◎Music"
+__mod_name__ = "Music"
 
 __help__ = """
-• `/song`** <songname artist(optional)>: download the song in it's best quality available.(API BASED)
-• `/video`** <songname artist(optional)>: download the video song in it's best quality available.
-• `/deezer`** <songname>: download from deezer
-• `/lyrics`** <songname artist(optional)>: sends the complete lyrics of the song provided as input
-• `/glyrics`** <i> song name </i> : This plugin searches for song lyrics with song name and artist.
+/video <i>query</i>: download video from youtube. 
+/deezer <i>query</i>: download from deezer. 
+/saavn <I>query</i>: download song from saavn. 
+/music <i>query</i>: download song from yt servers. (API BASED) 
+/lyrics <i>song name</i> : This plugin searches for song lyrics with song name.
+/glyrics <i> song name </i> : This plugin searches for song lyrics with song name and artist.
 """
-
